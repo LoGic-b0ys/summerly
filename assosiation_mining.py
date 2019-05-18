404116e4e10a1cb31c97e43b0c27c4094e375b2b
Logic Boys <yanruziqa@gmail.com>
2019-05-18 15:11:05 +0700

    Save the feature to the database

--- assosiation_mining.py
+++ assosiation_mining.py
@@ -11,9 +11,7 @@ for row in cursor:
 	if len(transaction) > 0 :
 		transaction_list.append(transaction)
 
-conn.close()
-
-association_rules = apriori(transaction_list, min_support=0.005, min_confidence=0, min_lift=3, min_length=2)
+association_rules = apriori(transaction_list, min_support=0.001, min_confidence=0, min_lift=3, min_length=2)
 association_results = list(association_rules)
 
 features = []
@@ -25,4 +23,17 @@ for item in association_results:
 	items = [x for x in pair]
 	features.append(items[0])
 
-print(features)
+features = set(features)
+
+sql = '''insert into feature(id_anime, feature) values(?,?)'''
+feature_text = ''
+
+for feature in features :
+	feature_text += feature + '|'
+
+cnt = conn.cursor()
+
+cnt.execute(sql, (5114, feature_text))
+conn.commit()
+
+conn.close()
